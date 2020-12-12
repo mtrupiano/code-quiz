629faba4d67ced2b5a89acc26daab28577ef1ddc
mtrupiano <markt6749@gmail.com>
2020-12-12 11:30:19 -0800

    more logic drafting; some styling

--- js/script.js
+++ js/script.js
@@ -10,43 +10,58 @@ var questions = [
         wrongs: ["wrong1", "wrong2", "wrong3"],
         used: false
     }, {
-        prompt: "How old are you",
+        prompt: "How old are you?",
         answer: "2222",
         wrongs: ["wrong1", "wrong2", "wrong3"],
         used: false
     }, {
-        prompt: "Hello, how are you?",
+        prompt: "Hello, I love you, won't you tell me your name?",
         answer: "Fine, thanks.",
         wrongs: ["wrong1", "wrong2", "wrong3"],
         used: false
     }
 ];
 
-var homeContainer = document
+var startButton     = document.querySelector("#start-btn");
+var hiscoreButton   = document.querySelector("#hiscore-btn");
+var promptHeaderEl  = document.querySelector("#prompt-header");
+var answerListEl    = document.querySelector("#answer-list");
+var timerEl         = document.querySelector("#timer");
 
-var startButton = document.querySelector("#start-btn");
-var hiscoreButton = document.querySelector("#hiscore-btn");
+var usedQuestions = [];
+var thisQuestion;
 
-var promptHeader = document.querySelector("#prompt-header");
-var answerList = document.querySelector("#answer-list");
-
-var timerElement = document.querySelector("#timer");
+answerListEl.addEventListener("click", function (event) {
+    if (event.target.textContent === thisQuestion.answer) {
+        // show "correct"
+        correct = true;
+    } else {
+        // show "incorrect"
+        correct = false;
+    }
+});
 
 function showQuestion() {
     // clear the question header and list
-    promptHeader.innerHTML = "";
-    answerList.innerHTML = ""; 
+    promptHeaderEl.innerHTML = "";
+    answerListEl.innerHTML = ""; 
 
-    var thisQuestion = questions[0];
+    // Select a random question from the question pool
+    var qIdx;
+    do {
+        qIdx = Math.floor(Math.random() * questions.length)
+    } while (usedQuestions.includes(qIdx));
+    usedQuestions.push(qIdx);
+    thisQuestion = questions[qIdx];
 
-    promptHeader.textContent = thisQuestion.prompt;
+    promptHeaderEl.textContent = thisQuestion.prompt;
 
     var choices = [thisQuestion.answer];
     for (var i = 0; i < thisQuestion.wrongs.length; i++) {
         choices.push(thisQuestion.wrongs[i]);
     }
 
-    var used = [];
+    var usedChoices = [];
     for (var i = 0; i < choices.length; i++) {
         // create a button element
         // create list item element
@@ -59,25 +74,27 @@ function showQuestion() {
             idx = Math.floor(Math.random() * choices.length)
         } while (used.includes(idx));
 
-        used.push(idx);
+        usedChoices.push(idx);
         newBtnEl.innerHTML = choices[idx];
 
         var liEl = document.createElement("li");
         liEl.setAttribute("class", "list-group-item");
         liEl.appendChild(newBtnEl);
-        answerList.appendChild(liEl);
+        answerListEl.appendChild(liEl);
     }
 
+    return correct;
 }
 
 function runQuiz() {
     startButton.style.display = "none";
     hiscoreButton.style.justifyContent = "right";
-    showQuestion();
+
+    
     var secondsLeft = 120;
     var timerInterval = setInterval(function () {
         secondsLeft--;
-        timerElement.textContent = `${secondsLeft} seconds remaining`;
+        timerEl.textContent = `${secondsLeft} seconds remaining`;
 
         if (secondsLeft === 0) {
             clearInterval(timerInterval);
@@ -86,6 +103,9 @@ function runQuiz() {
 
     }, 1000);
 
+    for (var i = 0; i < questions.length; i++) {
+        showQuestion();
+    }
 
 }
 
